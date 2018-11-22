910c2105dbaec0254af7ab7579ca297c3321c74c
Milad Doostan <20668135+miladdoostan@users.noreply.github.com>
2018-11-21 20:10:24 -0500

    Update train_test_split_function.R

--- train_test_split_function.R
+++ train_test_split_function.R
@@ -1,7 +1,10 @@
 # A simple function for splitting a tibble to training and testing sets based on a fraction
 
-train_test_split <- function(df, training_frac){
+train_test_split <- function(df, training_frac, seed=NA){
     library(tidyverse)
+    if (!is.na(seed)){
+        set.seed(seed)
+    }
     training <- df %>% mutate(id = row_number()) %>% sample_frac(training_frac)
     testing <- df %>% mutate(id = row_number()) %>% anti_join(training, by="id")
     return(list(training = training, testing = testing) %>% map(~select(.,-id)))
