0ee963deeb7a9f5570c74848c04d496829f0d8c1
Milad Doostan <20668135+miladdoostan@users.noreply.github.com>
2018-11-22 13:08:48 -0500

    Create knn_clf.R

---
+++ knn_clf.R
@@ -0,0 +1,18 @@
+# Fit a KNN classifier to a tibble using k-fold cross-validation and outputs the confusion matrices for all folds.
+# predictors and response take char vectors.
+
+k_nearest_neighbors <- function(df, predictors, response, k=5, folds=5, seed=NA){
+    splited_df <- split_df(df, folds, seed)
+    cm <- list()
+    for (i in 1:folds){
+        df_train <- splited_df[-i] %>% bind_rows() %>% select(predictors, response)
+        df_test <- splited_df[[i]] %>% select(predictors, response)
+        X_train <- df_train %>% select(predictors) %>% as.matrix()
+        X_test <- df_test %>% select(predictors) %>% as.matrix()
+        y_train <- df_train %>% pull(response) %>% as.factor()
+        testing_cm <- df_test %>% mutate(pred = knn(X_train, X_test, y_train, k=k)) %>% 
+        select(response, pred) %>% table()
+        cm[[i]] <- testing_cm
+    }
+    return(cm)   
+}
