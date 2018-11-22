e15b295686f59f947104045a18d8cacd026d8431
Milad Doostan <20668135+miladdoostan@users.noreply.github.com>
2018-11-22 13:17:10 -0500

    Update knn_clf.R

--- knn_clf.R
+++ knn_clf.R
@@ -4,6 +4,7 @@
 k_nearest_neighbors <- function(df, predictors, response, k=5, folds=5, seed=NA){
     library(tidyverse)
     source("split_df.R")
+    import::from(class, knn)
     splited_df <- split_df(df, folds, seed)
     cm <- list()
     for (i in 1:folds){
