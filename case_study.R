42abb5465aa75699b6060ec5ea6e8cf9ba6a37e8
Milad Doostan <20668135+miladdoostan@users.noreply.github.com>
2018-11-22 13:21:07 -0500

    Update case_study.R

--- case_study.R
+++ case_study.R
@@ -1,8 +1,29 @@
+# importing necessary libraries and functions
 library(tidyverse)
 library(ISLR)
+source("logistic_regression.R")
+source("knn_clf.R")
+
+# importing and tidying the Auto dataframe -  creating a binary variable as response ("mpg01")
 df <- Auto %>% as_tibble()
 df  <- df %>% mutate(cylinders =  factor(cylinders, levels=c(3,4,5,6,8), ordered = T),
                      origin = as.factor(origin),
                      year = as.integer(year),
                      mpg01 = as.factor(ifelse(mpg >= median(mpg), 1, 0)))
-head(df)
+print(head(df))
+
+# defining the predictors and response variables for binary classification
+predictors <-c("horsepower", "origin", "weight", 'cylinders', 'year', 'displacement', "acceleration")
+response <- c("mpg01")
+
+# building LR nad KNN classifiers
+LR_cm <- k_nearest_neighbors(df, predictors, response)
+knn_cm <- k_nearest_neighbors(df, predictors, response)
+
+# calculating the average accuracy of cross-validation sets
+LR_acc <- map_dbl(LR_cm, function(x){sum(diag(x))/sum(x)})
+knn_acc <- map_dbl(knn_cm, function(x){sum(diag(x))/sum(x)})
+
+# printing the results
+print(paste("Logistic regression average accuracy: ", mean(LR_acc)))
+print(paste("KNN average accuracy: ", mean(knn_acc)))
