c56c0f191c2555dc279ad3009fdaf965a26cc731
Milad Doostan <20668135+miladdoostan@users.noreply.github.com>
2018-11-22 13:48:52 -0500

    Create standardize.R

---
+++ standardize.R
@@ -0,0 +1,6 @@
+# A function that standardize (a.k.a. normaize) the numeric variables of a tibble and outputs the scaled tibble.
+
+standardize_df <- function(df, vars){
+library(tidyverse)
+  df %>% mutate_each_(funs(scale),vars=vars)
+}
